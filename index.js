f23a91604dab7042e5f88c406a476bc776a94c73
cyrilkennedy <kennedycyrilchimankpam@gmail.com>
2025-06-22 12:03:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+const a = 90
+if(a <70 ){
+    console.log("Excellent"); 
+}
+else if(a>=70 && a >=70){
+    console.log("Excellent"); 
+}
+
+
+const b = 69
+if(b <= 60 && b >=69 ){
+    console.log("Very Good"); 
+}
+else if(b >= 60 ){
+    console.log("Very Good"); 
+}
+
+
+const c = 59
+if(c >=51 ){
+    console.log("Good"); 
+}
+else if(c <= 50 && c >= 55){
+    console.log("Good"); 
+}
+
+
+const d = 49
+if(b <= 49 && b >=40 ){
+    console.log("Pass"); 
+}
+else if(d >= 40 ){
+    console.log("pass"); 
+}
+
+const f = 39
+if(f >=10 ){
+    console.log("Fail"); 
+}
+else if(f <= 39 && f >= 12){
+    console.log("Fail"); 
+}
+
+
+const jss1 = ["Marvel", "Abigail", "ihuoma", "Precious", "Ihechiluru"];
+const jss2 = ["Charles", "Tom", "Gabriel", "Carson", "Benjamin"];
+const jss3 = ["Alimat", "Hazan", "Hazard", "Joy", "Caroline"];
+const ss1 = ["Henry", "Lade", "Ubong", "Gift", "Linda"];
+const ss2 = ["Ann", "Lydia", "Judas", "Chillwel", "Jordan"];
+const ss3 = ["Fredico", "Kylian", "Bruno", "Kevin", "Cristiano"];
+
+for(let a =0; a < jss1.length; a++){console.log(jss1[a]);
+}
+
+for(let b =0; b < jss2.length; b++){console.log(jss2[b]);
+
+}for(let c =0; c < jss3.length; c++){console.log(jss3[c]);
+}
+
+for(let d =0; d < ss1.length; d++){console.log(ss1[d]);
+}
+
+for(let e =0; e < ss2.length; e++){console.log(ss2[e]);
+}
+
+for(let f =0; f < ss3.length; f++){console.log(ss3[f]);
+}
